4c6f1d64cdd1d85842ec5d703c20d70aac5820d3
kyanh <kyanh@2864bf00-ae8a-11dd-aca0-c9a4f5028835>
2008-11-13 18:18:25 +0000

    fix stuffs
    git-svn-id: file:///home/svn/vnmik4@82 2864bf00-ae8a-11dd-aca0-c9a4f5028835

--- makepatch.sh
+++ makepatch.sh
@@ -3,6 +3,10 @@
 ddir=`pwd`/distro/
 diffdir=$ddir/tmp
 
+msg() {
+	echo -e "::: $* :::"
+}
+
 if [ -z "$2" ]; then
 	echo "makepatch.sh <version1> <version2>"
 else
@@ -25,40 +29,51 @@ else
 		diff_er=$diffdir/diff-$v1-$v2.differ
 		diff_rm=$diffdir/diff-$v1-$v2.removed
 		patch_name=vnmik_${vv1}_${vv2}_patch
+		patch_file=$ddir/$patch_name.4
 
-		# rm -rf $ddir/tmp/{$afile,$bfile}
-		# unzip -u $ddir/$afile -d $ddir/tmp/$afile
-		# unzip -u $ddir/$bfile -d $ddir/tmp/$bfile
+		msg "remove old directories"
+		rm -rf $ddir/tmp/{$afile,$bfile}
+		msg "uncompress the source file"
+		unzip -qq -u $ddir/$afile -d $ddir/tmp/$afile
+		unzip -qq -u $ddir/$bfile -d $ddir/tmp/$bfile
 
 		cd $ddir/tmp
 
+		msg "creating diff log"
 		diff -r $afile $bfile > $diff_log
 
+		msg "find different files"
 		grep '^diff -r' $diff_log \
 			| gawk '{print $4}' \
 			| gawk -F '.zip/vnmik/' '{print $2}' \
 				> $diff_er
 
+		msg "find different binary files"
 		grep '^Binary files' $diff_log \
 			| gawk '{print $5}'\
 			| gawk -F '.zip/vnmik/' '{print $2}' \
 			>> $diff_er
 
+		msg "find files only in $v2"
 		grep "^Only in vnmik-$v2.zip" $diff_log >> $diff_er
 		sed -i 's#Only in ##g' $diff_er
 		sed -i 's#: #/#g' $diff_er
 		sed -i 's#vnmik-'$v2'.zip/vnmik/##g' $diff_er
 
-		grep "^Only in vnmik-$v1.zip" $diff_log > $diff_rm
+		msg "find files only in $v1"
+		grep "^Only in vnmik-$v1.zip" $diff_log
 		if [ $? -eq 0 ]; then
-			script=z.$patch_name
+			msg "some files should be removed. create the script now"
 
+			script="z.$patch_name"
+
+			grep "^Only in vnmik-$v1.zip" $diff_log > $diff_rm
 			sed -i 's#Only in ##g' $diff_rm
 			sed -i 's#: #/#g' $diff_rm
 			sed -i 's#vnmik-'$v1'.zip/vnmik/##g' $diff_rm
 
 			echo "${patch_name}_install() {" > $script
-
+			msg "generate scripts to remove files"
 			for f in `cat $diff_rm`; do
 				echo "stat_log going to remove $f" >> $script
 				echo "rm \$PREFIX/$f" >> $script
@@ -75,7 +90,8 @@ else
 		fi
 
 		cd $ddir/tmp/$bfile/vnmik
-		tar cfvj $ddir/$patch_name.4 `cat $diff_er`
-
+		msg "create the vnmik package"
+		tar cfj $patch_file `cat $diff_er`
+		msg "patch file is $patch_file"
 	fi
 fi
