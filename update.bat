124ff6943c6862b210a255572b85a29d4e3b2b47
kyanh <kyanh@2864bf00-ae8a-11dd-aca0-c9a4f5028835>
2008-11-16 08:38:09 +0000

    update info
    git-svn-id: file:///home/svn/vnmik4@89 2864bf00-ae8a-11dd-aca0-c9a4f5028835

--- update.bat
+++ update.bat
@@ -1,6 +1,5 @@
 @echo off
-rem This file is part of VnMiK 4.0.0 <http://vnmik.sf.net/>
+rem This file is part of VnMiK 4.x <http://vnmik.sf.net/>
 set PATH=%~dp0\bin;%PATH%
 set HOME=%~dp0
-rem call user.cfg.bat
 bash vnmik.update
