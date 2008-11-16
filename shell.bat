39127d2c1c226dd8edd5fb75fdac9131ccc8da2a
kyanh <kyanh@2864bf00-ae8a-11dd-aca0-c9a4f5028835>
2008-11-16 08:35:13 +0000

    drop svn subsystem: we use linux to develop vnmik
    git-svn-id: file:///home/svn/vnmik4@88 2864bf00-ae8a-11dd-aca0-c9a4f5028835

--- shell.bat
+++ shell.bat
@@ -1,6 +1,6 @@
 @echo off
 rem This file is part of VnMiK 4.0.0 <http://vnmik.sf.net/>
-set PATH=%~dp0\bin;%~dp0\bin\svn;c:\cygwin\bin;%PATH%
+set PATH=%~dp0\bin;%PATH%
 set HOME=%~dp0
 set PS1=vnmik \[\033[0;1;34m\]\W\[\033[0;0;37m\] $ 
 bash --login -i 
