34626f4f49ea149261b4acf6f7b2fb84ece221d3
kyanh <kyanh@2864bf00-ae8a-11dd-aca0-c9a4f5028835>
2008-12-03 13:31:22 +0000

    delete stupid char
    git-svn-id: file:///home/svn/vnmik4@97 2864bf00-ae8a-11dd-aca0-c9a4f5028835

--- test.bat
+++ test.bat
@@ -2,4 +2,5 @@
 rem This file is part of VnMiK 4.x <http://vnmik.sf.net/>
 set PATH=%~dp0\bin;%PATH%
 set HOME=%~dp0
-bash vnmik.testpause
+bash vnmik.test
+pause
--- update.bat
+++ update.bat
@@ -3,4 +3,5 @@ rem This file is part of VnMiK 4.x <http://vnmik.sf.net/>
 set PATH=%~dp0\bin;%PATH%
 set HOME=%~dp0
 bash vnmik.update
-bash vnmik.update.corepause
+bash vnmik.update.core
+pause
